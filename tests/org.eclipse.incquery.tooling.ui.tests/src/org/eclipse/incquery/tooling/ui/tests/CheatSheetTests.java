a66a75c930ae8ac8f5e07762f5cc405c3508a795
Rudi Szabó <mail@szaborudi.hu>
2013-03-06 18:20:05 +0100

    CheatSheets tesztek összevonva, egy teszt alatt úgyis kibukik mind

--- tests/org.eclipse.incquery.tooling.ui.tests/src/org/eclipse/incquery/tooling/ui/tests/CheatSheetTests.java
+++ tests/org.eclipse.incquery.tooling.ui.tests/src/org/eclipse/incquery/tooling/ui/tests/CheatSheetTests.java
@@ -1,67 +1,40 @@
 package org.eclipse.incquery.tooling.ui.tests;
 
-import static org.junit.Assert.assertNotNull;
-
 import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
-import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotView;
-import org.eclipse.swtbot.swt.finder.widgets.SWTBotTree;
-import org.eclipse.swtbot.swt.finder.widgets.SWTBotTreeItem;
+import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
+import org.eclipse.swtbot.swt.finder.utils.SWTBotPreferences;
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+@RunWith(SWTBotJunit4ClassRunner.class)
 public class CheatSheetTests {
 
 	private static SWTWorkbenchBot bot = new SWTWorkbenchBot();
 	private static String GETTING_STARTED = "Getting Started with EMF-IncQuery";
 	private static String INCQUERY = "EMF-IncQuery";
 	private static String CHEATSHEET = "Cheat Sheets...";
-
+	
 	@BeforeClass
 	public static void beforeClass(){
 		bot.viewByTitle("Welcome").close();
+		SWTBotPreferences.PLAYBACK_DELAY = 200;
 	}
 	
 	@Test
 	public void testIncQCheatsheetsExists() {
-		// Arrange
 		bot.menu("Help").menu(CHEATSHEET).click();
+		bot.shell("Cheat Sheet Selection").activate();
+		bot.tree().getTreeItem(INCQUERY).expand().select(GETTING_STARTED);
+		bot.button("OK").click();
+		bot.viewByTitle("Cheat Sheets")
+			.bot().treeWithLabel(GETTING_STARTED);
+	}
+	
 
-        // Act
-		SWTBotTree tree = bot.tree();
-		SWTBotTreeItem item = tree.getTreeItem(INCQUERY);
-		bot.button("Cancel").click();
-		
-        // Assert
-		assertNotNull(item);
-	}
-	
-	@Test
-	public void testIncQCheatsheeetsOpen(){
-		// Arrange
-		bot.menu("Help").menu(CHEATSHEET).click();
-		bot.shell("Cheat Sheet Selection").activate();
-		bot.tree().getTreeItem(INCQUERY).expand().select(GETTING_STARTED);
-		bot.button("OK").click();
-		
-		// Act
-		SWTBotView cheatsheet = bot.viewByTitle("Cheat Sheets");
-		
-		// Assert
-		assertNotNull(cheatsheet);
-	}
-	
-	@Test
-	public void testIncQCheatsheeetsShows(){
-		// Arrange
-		bot.menu("Help").menu(CHEATSHEET).click();
-		bot.tree().getTreeItem(INCQUERY).expand().select(GETTING_STARTED);
-		bot.shell("Cheat Sheet Selection").activate();
-		bot.button("OK").click();
-		
-		// Act
-		SWTBotTree tree = bot.treeWithLabel(GETTING_STARTED);
-		
-		// Assert
-		assertNotNull(tree);
-	}
+    @AfterClass
+    public static void sleep() {
+    	//bot.sleep(2000);
+    }
 }
