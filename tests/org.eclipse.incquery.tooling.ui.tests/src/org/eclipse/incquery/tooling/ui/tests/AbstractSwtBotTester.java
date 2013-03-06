d24f4c33abbb232d0f0561d93b2790cac582c9fc
Rudi Szabó <mail@szaborudi.hu>
2013-03-06 18:22:32 +0100

    AbstractSwtBotTester-be kiemelhetők közös részek

---
+++ tests/org.eclipse.incquery.tooling.ui.tests/src/org/eclipse/incquery/tooling/ui/tests/AbstractSwtBotTester.java
@@ -0,0 +1,65 @@
+package org.eclipse.incquery.tooling.ui.tests;
+
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
+import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotView;
+import org.eclipse.swtbot.swt.finder.finders.UIThreadRunnable;
+import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
+import org.eclipse.swtbot.swt.finder.results.VoidResult;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.WorkbenchException;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+
+/*
+ * From: http://cataquavice.svn.sourceforge.net/viewvc/cataquavice/trunk/tests/de.abg.jreichert.serviceqos.ui.test/src/de/abg/jreichert/serviceqos/ui/swtbottest/AbstractSwtBotTester.java?revision=439&view=markup
+ */
+
+@RunWith(SWTBotJunit4ClassRunner.class)
+public abstract class AbstractSwtBotTester {
+
+    protected static SWTWorkbenchBot bot;
+    
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        bot = new SWTWorkbenchBot();
+        SWTBotView view = bot.activeView();
+        if(view != null && view.getTitle().equals("Welcome")) {
+        	view.close();
+        }
+    }
+
+    @AfterClass
+    public static void destroy() {
+        UIThreadRunnable.syncExec(new VoidResult() {
+            public void run() {
+                resetWorkbench();
+            }
+        });
+    }
+
+    private static void resetWorkbench() {
+        try {
+            IWorkbench workbench = PlatformUI.getWorkbench();
+            IWorkbenchWindow workbenchWindow =
+                    workbench.getActiveWorkbenchWindow();
+            IWorkbenchPage page = workbenchWindow.getActivePage();
+            Shell activeShell = Display.getCurrent().getActiveShell();
+            if (activeShell != workbenchWindow.getShell()) {
+                activeShell.close();
+            }
+            page.closeAllEditors(false);
+            String defaultPerspectiveId =
+                    workbench.getPerspectiveRegistry().getDefaultPerspective();
+            workbench.showPerspective(defaultPerspectiveId, workbenchWindow);
+            page.resetPerspective();
+        } catch (WorkbenchException e) {
+            throw new RuntimeException(e);
+        }
+    }    
+}
--- tests/org.eclipse.incquery.tooling.ui.tests/src/org/eclipse/incquery/tooling/ui/tests/CheatSheetTests.java
+++ tests/org.eclipse.incquery.tooling.ui.tests/src/org/eclipse/incquery/tooling/ui/tests/CheatSheetTests.java
@@ -1,40 +1,19 @@
 package org.eclipse.incquery.tooling.ui.tests;
 
-import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
-import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
-import org.eclipse.swtbot.swt.finder.utils.SWTBotPreferences;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
-@RunWith(SWTBotJunit4ClassRunner.class)
-public class CheatSheetTests {
+public class CheatSheetTests extends AbstractSwtBotTester {
 
-	private static SWTWorkbenchBot bot = new SWTWorkbenchBot();
 	private static String GETTING_STARTED = "Getting Started with EMF-IncQuery";
 	private static String INCQUERY = "EMF-IncQuery";
 	private static String CHEATSHEET = "Cheat Sheets...";
 	
-	@BeforeClass
-	public static void beforeClass(){
-		bot.viewByTitle("Welcome").close();
-		SWTBotPreferences.PLAYBACK_DELAY = 200;
-	}
-	
 	@Test
 	public void testIncQCheatsheetsExists() {
 		bot.menu("Help").menu(CHEATSHEET).click();
-		bot.shell("Cheat Sheet Selection").activate();
 		bot.tree().getTreeItem(INCQUERY).expand().select(GETTING_STARTED);
 		bot.button("OK").click();
 		bot.viewByTitle("Cheat Sheets")
 			.bot().treeWithLabel(GETTING_STARTED);
 	}
-	
-
-    @AfterClass
-    public static void sleep() {
-    	//bot.sleep(2000);
-    }
 }
