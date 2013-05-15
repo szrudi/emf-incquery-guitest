146b834eb82a90e69113f24a487ff2b451704742
Rudi Szabó <mail@szaborudi.hu>
2013-05-16 00:27:21 +0200

    SWTBot tesztek átírása, hogy kövessék a diplomatervet

--- tests/org.eclipse.incquery.tooling.ui.tests/src/org/eclipse/incquery/tooling/ui/tests/SchoolTests.java
+++ tests/org.eclipse.incquery.tooling.ui.tests/src/org/eclipse/incquery/tooling/ui/tests/SchoolTests.java
@@ -1,6 +1,7 @@
 package org.eclipse.incquery.tooling.ui.tests;
 
 import static org.eclipse.swtbot.swt.finder.waits.Conditions.shellCloses;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
@@ -12,9 +13,7 @@ import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotEclipseEditor;
 import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotView;
 import org.eclipse.swtbot.swt.finder.SWTBot;
 import org.eclipse.swtbot.swt.finder.exceptions.WidgetNotFoundException;
-import org.eclipse.swtbot.swt.finder.utils.FileUtils;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
-import org.eclipse.swtbot.swt.finder.widgets.SWTBotToolbarDropDownButton;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotTree;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotTreeItem;
 import org.eclipse.swtbot.swt.finder.widgets.TimeoutException;
@@ -26,26 +25,11 @@ import org.jnario.runner.Order;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.osgi.framework.Bundle;
 
-@RunWith(FeatureRunner.class)
 public class SchoolTests extends AbstractIncQueryTester{
 	protected static String PROJECTNAME = "school.tests";
 	protected static String EIQFILE = "schoolQueries.eiq";
 	protected static String MODELFILE = "BUTE.school";
-   
-	@Test
-	@Order(0)
-	public void createIncQueryProject() {
-		bot.menu("File").menu("New").menu("Project...").click();
-		SWTBotShell shell = bot.shell("New Project");
-		shell.activate();
-		bot.tree().expandNode("EMF-IncQuery","EMF-IncQuery Project").select();
-		bot.button("Next >").click();
-		bot.textWithLabel("Project name:").setText(PROJECTNAME);
-		bot.button("Finish").click();
-		bot.waitUntil(shellCloses(shell), 20000);
-	}
 	
 	@Test
 	@Ignore
@@ -84,9 +68,151 @@ public class SchoolTests extends AbstractIncQueryTester{
 		bot.viewByTitle("Project Explorer").bot().tree()
 			.expandNode("projectName");
 	}
+   
+	@Test
+	@Order(0)
+	public void createIncQueryProject() {
+		boolean isCreated = false;
+		bot.menu("File").menu("New").menu("Project...").click();
+		SWTBotShell shell = bot.shell("New Project");
+		shell.activate();
+		bot.tree().expandNode("EMF-IncQuery","EMF-IncQuery Project").select();
+		bot.button("Next >").click();
+		bot.textWithLabel("Project name:").setText(PROJECTNAME);
+		bot.button("Finish").click();
+		bot.waitUntil(shellCloses(shell), 20000);
+		setupProjectDependency();
+		try {
+			bot.viewByTitle("Project Explorer").bot().tree()
+				.expandNode(PROJECTNAME);
+			isCreated = true;
+		} catch (WidgetNotFoundException e) {
+			isCreated = false;
+		}
+		
+		assertTrue("Project with name \""+PROJECTNAME+"\" is not created", isCreated);
+
+	}
 	
 	@Test
-	@Order(1)
+	@Order(2)
+	public void createNewEiq() {
+		// Arrange
+		bot.menu("File").menu("New").menu("Other...").click();
+		SWTBotShell shell = bot.shell("New");
+		shell.activate();
+		bot.tree().expandNode("EMF-IncQuery")
+			.select("EMF-IncQuery Query Definition");
+		bot.button("Next >").click();
+		
+		// Act
+		bot.textWithLabel("File name:").setText(EIQFILE);
+		bot.button("Next >").click();
+		bot.textWithLabel("Pattern name:").setText("testPattern");
+		bot.button("Finish").click();
+		bot.waitUntil(shellCloses(shell), 15000);
+		
+		// Assert
+		SWTBotTreeItem item; 
+		try {
+			item = bot.viewByTitle("Project Explorer").bot()
+				.tree().expandNode("school.tests", "src", EIQFILE);
+		} catch (WidgetNotFoundException e){
+			item = null;
+		}
+		assertNotNull(EIQFILE + " was not created!", item);
+	}
+	
+	// TODO Add gombot meg kellene találni valahogy
+	//bot.buttonWithLabel("Add").click();
+	//bot.tree().select("http://school.ecore");
+	//bot.button("OK").click();
+	
+	@Test
+	@Order(3)
+	public void openQueryExplorer() {
+		bot.menu("Window").menu("Show View").menu("Other...").click();
+		SWTBotShell shell = bot.shell("Show View");
+		shell.activate();
+		
+		bot.tree().expandNode("EMF-IncQuery").select("Query Explorer");
+		bot.button("OK").click();
+
+		bot.waitUntil(shellCloses(shell), 5000);
+		boolean queryExplorerIsOpen;
+		try {
+			bot.viewByTitle("Query Explorer");
+			queryExplorerIsOpen = true;
+		} catch (WidgetNotFoundException e){
+			queryExplorerIsOpen = false;
+		}
+		
+		assertTrue("Could not open Query Explorer", queryExplorerIsOpen);
+	}
+		
+	@Test
+	@Order(4)
+	public void loadModelToExplorerWithPlayButton(){
+		createFolderInProject("model", PROJECTNAME);
+
+		addFileFromBundleToFolder(MODELFILE,"test-files/BUTE.school",PROJECTNAME+"/model");
+		getTreeItemFromProject(PROJECTNAME, "model", MODELFILE)
+				.contextMenu("Open With").menu("School Model Editor").click();
+		Matcher<IEditorReference> withPartName = WithPartName
+				.withPartName(Matchers.containsString(MODELFILE));		
+		bot.waitUntil(Conditions.waitForEditor(withPartName));
+		
+		//pressPlayButtonInQueryExplorer();
+		SWTBotView queryExplorerBot = bot.viewByTitle("Query Explorer");
+		queryExplorerBot.toolbarDropDownButton("Load model").click();
+		
+		// assert
+		SWTBotTree middleTree = bot.viewByTitle("Query Explorer").bot().tree(1);
+		try {
+			middleTree.getTreeItem("[platform:/resource/"+
+				PROJECTNAME+"/model/"+
+				MODELFILE+"][school.presentation.SchoolEditorID]");
+		} catch (WidgetNotFoundException e){
+			fail("Model is not loaded to Query Explorer: "+MODELFILE);
+		}
+	}
+	
+	@Test
+	@Order(5)
+	public void setAndLoadTestPattern() {
+		String pattern = "pattern schools(Sch) = { School(Sch); }";
+		
+		getTreeItemFromProject(PROJECTNAME,"src",EIQFILE).select().doubleClick();
+		
+		Matcher<IEditorReference> withPartName = WithPartName
+				.withPartName(Matchers.containsString(EIQFILE));
+		
+		bot.waitUntil(Conditions.waitForEditor(withPartName));
+		SWTBotEclipseEditor editor = bot.editorByTitle(EIQFILE).toTextEditor();
+		editor.setText(getText("package "+PROJECTNAME,"import \"http://school.ecore\"",pattern));
+		editor.save();
+		
+		SWTBotView queryExplorerBot = bot.viewByTitle("Query Explorer");
+		queryExplorerBot.toolbarDropDownButton("Load model").click();
+
+		// assert
+		SWTBotTree tree = bot.viewByTitle("Query Explorer").bot().tree(0);
+		tree.expandNode("Runtime", PROJECTNAME, "schools");	
+
+		tree = bot.viewByTitle("Query Explorer").bot().tree(1);
+		SWTBotTreeItem[] items = tree.expandNode("[platform:/resource/"+
+				PROJECTNAME+"/model/"+
+				MODELFILE+"][school.presentation.SchoolEditorID]").getItems();
+		boolean found = false;
+		for (SWTBotTreeItem item : items){
+			if(item.getText().startsWith(PROJECTNAME+".schools")){
+				found = true;
+				break;
+			}
+		}
+		assertTrue(found);
+	}
+	
 	public void setupProjectDependency() {
 		SWTBot projectExplorerBot = bot.viewByTitle("Project Explorer").bot();
 		SWTBotTree tree = projectExplorerBot.tree();
@@ -112,158 +238,5 @@ public class SchoolTests extends AbstractIncQueryTester{
 
 		bot.viewByTitle("Project Explorer").show();
 	}
-	
-	@Test
-	@Order(2)
-	public void openQueryExplorer() {
-		bot.menu("Window").menu("Show View").menu("Other...").click();
-		SWTBotShell shell = bot.shell("Show View");
-		shell.activate();
-		
-		bot.tree().expandNode("EMF-IncQuery").select("Query Explorer");
-		bot.button("OK").click();
 
-		bot.waitUntil(shellCloses(shell), 5000);
-	}
-
-	@Test
-	@Order(3)
-	public void createModelFolder() {
-		bot.menu("File").menu("New").menu("Folder").click();
-		SWTBotShell shell = bot.shell("New Folder");
-		shell.activate();
-		bot.tree().select(PROJECTNAME);
-		bot.textWithLabel("Folder name:").setText("model");
-		bot.button("Finish").click();
-		bot.waitUntil(shellCloses(shell), 5000);
-	}
-	
-	@Test
-	@Order(4)
-	public void copyButeModel() {
-		bot.menu("File").menu("New").menu("File").click();
-		SWTBotShell shell = bot.shell("New File");
-		shell.activate();
-		bot.tree().expandNode(PROJECTNAME, "model").select();
-		bot.textWithLabel("File name:").setText(MODELFILE);
-		bot.button("Finish").click();
-		bot.waitUntil(shellCloses(shell), 30000);
-		
-		bot.editorByTitle(MODELFILE).close();
-		
-		getTreeItemFromProject(PROJECTNAME, "model", MODELFILE)
-				.contextMenu("Open With").menu("Text Editor").click();
-
-		Matcher<IEditorReference> withPartName = WithPartName
-				.withPartName(Matchers.containsString(MODELFILE));		
-		bot.waitUntil(Conditions.waitForEditor(withPartName));
-
-		Bundle bundle = Activator.getContext().getBundle();
-		String contents = FileUtils.read(bundle.getEntry("test-files/BUTE.school"));
-
-		SWTBotEclipseEditor editor = bot.editor(withPartName).toTextEditor();		
-		editor.setText(contents);
-		editor.saveAndClose();
-	}
-	
-	@Test
-	@Order(5)
-	public void createNewEiq() {
-		bot.menu("File").menu("New").menu("Other...").click();
-		SWTBotShell shell = bot.shell("New");
-		shell.activate();
-		bot.tree().expandNode("EMF-IncQuery")
-			.select("EMF-IncQuery Query Definition");
-		bot.button("Next >").click();
-		bot.textWithLabel("File name:").setText(EIQFILE);
-		bot.button("Next >").click();
-		bot.textWithLabel("Pattern name:").setText("testPattern");
-		
-		// TODO Add gombot meg kellene találni valahogy
-		//bot.buttonWithLabel("Add").click();
-		//bot.tree().select("http://school.ecore");
-		//bot.button("OK").click();
-		
-		bot.button("Finish").click();
-		bot.waitUntil(shellCloses(shell), 5000);
-	}
-	
-	@Test
-	@Order(6)
-	public void setTestPattern() {
-		String pattern = "pattern schools(Sch) = { School(Sch); }";
-		getTreeItemFromProject(PROJECTNAME,"src",EIQFILE).select().doubleClick();
-		
-		Matcher<IEditorReference> withPartName = WithPartName
-				.withPartName(Matchers.containsString(EIQFILE));
-		
-		bot.waitUntil(Conditions.waitForEditor(withPartName));
-		SWTBotEclipseEditor editor = bot.editorByTitle(EIQFILE).toTextEditor();
-		editor.setText(getText("package "+PROJECTNAME,"import \"http://school.ecore\"",pattern));
-        editor.saveAndClose();
-	}
-	
-	@Test
-	@Order(7)
-	public void loadModelToExplorerWithPlayButton(){
-		getTreeItemFromProject(PROJECTNAME, "model", MODELFILE)
-				.contextMenu("Open With").menu("School Model Editor").click();
-		Matcher<IEditorReference> withPartName = WithPartName
-				.withPartName(Matchers.containsString(MODELFILE));		
-		bot.waitUntil(Conditions.waitForEditor(withPartName));
-		
-		pressPlayButtonInQueryExplorer();
-		
-		// assert
-		SWTBotTree middleTree = bot.viewByTitle("Query Explorer").bot().tree(1);
-		try {
-			middleTree.getTreeItem("[platform:/resource/"+
-				PROJECTNAME+"/model/"+
-				MODELFILE+"][school.presentation.SchoolEditorID]");
-		} catch (WidgetNotFoundException e){
-			fail("Model is not loaded to Query Explorer: "+MODELFILE);
-		}
-	}
-	
-	@Test
-	@Order(8)
-	public void loadPatternsToExplorerWithPlayButton(){
-		getTreeItemFromProject(PROJECTNAME, "src", EIQFILE).select().doubleClick();
-		
-		Matcher<IEditorReference> withPartName = WithPartName
-				.withPartName(Matchers.containsString(EIQFILE));		
-		bot.waitUntil(Conditions.waitForEditor(withPartName),15*1000);
-		
-		pressPlayButtonInQueryExplorer();	
-
-		// assert
-		SWTBotTree tree = bot.viewByTitle("Query Explorer").bot().tree(0);
-		tree.expandNode("Runtime", PROJECTNAME, "schools");	
-
-		tree = bot.viewByTitle("Query Explorer").bot().tree(1);
-		SWTBotTreeItem[] items = tree.expandNode("[platform:/resource/"+
-				PROJECTNAME+"/model/"+
-				MODELFILE+"][school.presentation.SchoolEditorID]").getItems();
-		boolean found = false;
-		for (SWTBotTreeItem item : items){
-			if(item.getText().startsWith(PROJECTNAME+".schools")){
-				found = true;
-				break;
-			}
-		}
-		assertTrue(found);
-	}
-	
-	private void pressPlayButtonInQueryExplorer(){
-		pressPlayButtonInQueryExplorer("click");
-	}
-	private void pressPlayButtonInQueryExplorer(String action){
-		SWTBotView queryExplorerBot = bot.viewByTitle("Query Explorer");
-		SWTBotToolbarDropDownButton playBtn = queryExplorerBot.toolbarDropDownButton("Load model");
-		if (action == "click"){
-			playBtn.click();
-		} else {
-			playBtn.menuItem(action).click();
-		}		
-	}
 }
--- tests/org.eclipse.incquery.tooling.ui.tests/src/org/eclipse/incquery/tooling/ui/tests/swtbot/helper/AbstractIncQueryTester.java
+++ tests/org.eclipse.incquery.tooling.ui.tests/src/org/eclipse/incquery/tooling/ui/tests/swtbot/helper/AbstractIncQueryTester.java
@@ -1,28 +1,45 @@
 package org.eclipse.incquery.tooling.ui.tests.swtbot.helper;
 
+import static org.eclipse.swtbot.swt.finder.waits.Conditions.shellCloses;
+
+import java.util.Arrays;
+
+import org.eclipse.incquery.tooling.ui.tests.Activator;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
+import org.eclipse.swtbot.eclipse.finder.matchers.WithPartName;
+import org.eclipse.swtbot.eclipse.finder.waits.Conditions;
+import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotEclipseEditor;
 import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotView;
 import org.eclipse.swtbot.swt.finder.finders.UIThreadRunnable;
 import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
 import org.eclipse.swtbot.swt.finder.results.VoidResult;
+import org.eclipse.swtbot.swt.finder.utils.FileUtils;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotToolbarDropDownButton;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotTreeItem;
+import org.eclipse.ui.IEditorReference;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.WorkbenchException;
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
+import org.jnario.runner.FeatureRunner;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
+import org.osgi.framework.Bundle;
 
 /*
  * From: http://cataquavice.svn.sourceforge.net/viewvc/cataquavice/trunk/tests/de.abg.jreichert.serviceqos.ui.test/src/de/abg/jreichert/serviceqos/ui/swtbottest/AbstractSwtBotTester.java?revision=439&view=markup
  */
 
-@RunWith(SWTBotJunit4ClassRunner.class)
-public class AbstractIncQueryTester {
+//@RunWith(SWTBotJunit4ClassRunner.class)
+@RunWith(FeatureRunner.class)
+public abstract class AbstractIncQueryTester {
 
     protected static SWTWorkbenchBot bot;
     
@@ -92,5 +109,60 @@ public class AbstractIncQueryTester {
 	protected SWTBotTreeItem getTreeItemFromProject(String... strings) {
 		return bot.viewByTitle("Project Explorer").bot().tree().expandNode(strings);
 	}
+	protected void createFolderInProject(String folder, String project) {
+		bot.menu("File").menu("New").menu("Folder").click();
+		SWTBotShell shell = bot.shell("New Folder");
+		shell.activate();
+		bot.tree().select(project);
+		bot.textWithLabel("Folder name:").setText(folder);
+		bot.button("Finish").click();
+		bot.waitUntil(shellCloses(shell), 5000);
+	}
+	public void addFileFromBundleToFolder(String file, String from, String to) {
+		bot.menu("File").menu("New").menu("File").click();
+		SWTBotShell shell = bot.shell("New File");
+		shell.activate();
+		bot.tree().expandNode(to.split("/")).select();
+		bot.textWithLabel("File name:").setText(file);
+		bot.button("Finish").click();
+		bot.waitUntil(shellCloses(shell), 30000);
+		
+		bot.editorByTitle(file).close();
+		
+		getTreeItemFromProject(append(to.split("/"),file))
+				.contextMenu("Open With").menu("Text Editor").click();
+
+		Matcher<IEditorReference> withPartName = WithPartName
+				.withPartName(Matchers.containsString(file));		
+		bot.waitUntil(Conditions.waitForEditor(withPartName));
+
+		Bundle bundle = Activator.getContext().getBundle();
+		String contents = FileUtils.read(bundle.getEntry(from));
+
+		SWTBotEclipseEditor editor = bot.editor(withPartName).toTextEditor();		
+		editor.setText(contents);
+		editor.saveAndClose();
+	}
 	
+	protected static <T> T[] append(T[] arr, T element) {
+	    final int N = arr.length;
+	    arr = Arrays.copyOf(arr, N + 1);
+	    arr[N] = element;
+	    return arr;
+	}
+	
+	/*
+	protected void pressPlayButtonInQueryExplorer(){
+		pressPlayButtonInQueryExplorer("click");
+	}
+	protected void pressPlayButtonInQueryExplorer(String action){
+		SWTBotView queryExplorerBot = bot.viewByTitle("Query Explorer");
+		SWTBotToolbarDropDownButton playBtn = queryExplorerBot.toolbarDropDownButton("Load model");
+		if (action == "click"){
+			playBtn.click();
+		} else {
+			playBtn.menuItem(action).click();
+		}		
+	}*/
+
 }
